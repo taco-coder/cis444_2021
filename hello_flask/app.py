82ed2d5bd1a5caa60c89192a93152ce34d21f588
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-10-18 16:26:09 -0700

    playing with post to create_creds

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -86,7 +86,7 @@ def hello_db():
     return json_response(a = first, b = second, c = third)
 
 #assignemnt 3 fullstack stuff
-@app.route('/create_creds', )
+@app.route('/create_creds', methods=['POST'])
 def create_creds():
     cur = db.cursor()
     inputForm = str(request.form)
