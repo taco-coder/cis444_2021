e640c3d819259eb1dcaccd9df829d7355cafe6cc
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-10-18 17:39:33 -0700

    added console to ajax call

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -91,10 +91,7 @@ def create_creds():
     cur = db.cursor()
     credsForm = request.form
     db.commit()
-    return json_response(create_status = "success")
-
-
-
+    return 0
 
 @app.route('/check_creds')
 def check_creds():
