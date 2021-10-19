1917c060f2c3ad8fbb9adae71a623ceb8b94d1fd
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-10-18 17:51:47 -0700

    made separate signup template page

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -86,6 +86,9 @@ def hello_db():
     return json_response(a = first, b = second, c = third)
 
 #assignemnt 3 fullstack stuff
+@app.route('/get_signup')
+def get_signup():
+    return render_template('signup.html')
 @app.route('/create_creds', methods=['POST'])
 def create_creds():
     cur = db.cursor()
