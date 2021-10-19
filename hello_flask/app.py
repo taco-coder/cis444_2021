c96406c7d65295866bba482130afb4e5fafe4946
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-10-18 17:22:23 -0700

    playing with create_status and getjson

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -94,7 +94,7 @@ def create_creds():
     cur = db.cursor()
     credsForm = request.form
     db.commit()
-    return json_response(create_status={"status": "success"})
+    return json_response(create_status={"response": "success"})
 
 @app.route('/check_creds')
 def check_creds():
