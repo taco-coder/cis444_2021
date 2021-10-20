b2c57994c3a0cda62c8186893f7b7f465eafbe6f
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-10-20 12:25:14 -0700

    encode password payload error

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -108,7 +108,7 @@ def create_creds():
     credsForm = request.form
     cur.execute("select * from users where username = '" + credsForm['username'] + "';")
     if cur.fetchone() is None:
-        jwt_pass = jwt.encode(credsForm['password'], JWT_SECRET, algorithm="HS256")
+        jwt_pass = jwt.encode({"password" :credsForm['password']}, JWT_SECRET, algorithm="HS256")
         print(jwt_pass)
         cur.execute("insert into users (username, password) values ('" + credsForm['username'] + "', '" + jwt_pass + "');")
         db.commit()
