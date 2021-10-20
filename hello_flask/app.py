4eddbf56bfd01bcf1f80bb2838c93f93bfc3c41c
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-10-19 20:36:24 -0700

    undoing those changes

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -121,12 +121,9 @@ def check_creds():
     if cur.fetchone() is None:
         return render_template("bookstore.html", account_status = "Incorrect username/password. Please try again.")
     else:
-        return get_main_page()
-        #return current_app.send_static_file("mainpage.html")
+        return current_app.send_static_file("mainpage.html")
 
 @app.route('/main_store')
-def get_main_page():
-    return current_app.send_static_file("mainpage.html")
 def main_page():
     cur = db.cursor()
     cur.execute("select * from books;")
