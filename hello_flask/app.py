1e845efb13610ea7a3b3709c2c8a3d07cfd7fe20
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-10-23 19:35:28 -0700

    debugging

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -147,7 +147,7 @@ def get_red_lepanka():
     cur.execute("select * from reviews where id = 1;")
     uReviews = cur.fetchall()
     print(uReviews)
-    return render_template("redlepanka.html", bookname=bResult[1], price=bResult[2], reviews=uReviews, numReviews = uReviews.size)
+    return render_template("redlepanka.html", bookname=bResult[1], price=bResult[2], reviews=uReviews, numReviews = len(uReviews))
 
 @app.route('/become_taco', methods=['GET'])    
 def get_taco():
