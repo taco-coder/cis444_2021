f823e82eac2ab264e923ed7337e7f44a78dae54c
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-10-25 17:13:09 -0700

    debugging cart page

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -189,7 +189,8 @@ def get_ego_bias():
 def get_cart():
     print(session['book_name'])
     print(jsonify(session['book_name']))
-    return render_template("cart.html")
+    return json_response(output = jsonify(session['book_name']))
+    # return render_template("cart.html")
 
 @app.route('/add_to_cart', methods=['POST', 'GET'])
 def add_to_cart():
@@ -197,9 +198,9 @@ def add_to_cart():
         session['book_name'] = session.get('book_name') + request.form['book_name']
     else:
         session['book_name'] = request.form['book_name']
-    print(session['book_name'])
-    print(request.form['book_price'])
-    print(session['user'])
+    #print(session['book_name'])
+    #print(request.form['book_price'])
+    #print(session['user'])
     if request.referrer == "http://23.21.164.56/check_creds": #throws bad proxy error when I added the session code; on signin stays in /check_cred endpoint
         return redirect("/to_store")
     else:
