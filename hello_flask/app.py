291df7cadff626fe5aa47c91aae26be6db4007ba
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-10-30 13:29:26 -0700

    debugging

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -257,10 +257,10 @@ def place_order():
         cart_user = jwt.decode(session['user'], JWT_SECRET, algorithms=["HS256"])
         quantity = 1
         print(f"sorted: {cart_books}")
+        print(len(cart_books))
         for i in range(0, len(cart_books)):
             currentBook = cart_books[i]
             print(f"Current index: {i} and {currentBook}")
-    
         return json_response(status = "Order Placed!")
 
     return json_response(status = "Your cart is empty.")
