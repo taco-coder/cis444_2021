488c391c44a676cd416153eedc896dcc9e7c13a8
Ubuntu <cjardin@jardinlabs.com>
2021-11-13 00:56:22 +0000

    Small changes to help :)

--- flask_jwt_rest_server/tools/token_tools.py
+++ flask_jwt_rest_server/tools/token_tools.py
@@ -1,6 +1,8 @@
 import jwt
 import datetime
 from flask import  g
+from tools.logging import logger
+
 
 def create_token(token_data):
     token_data['exp'] = datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=30)
