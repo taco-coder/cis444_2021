e408329d0fdb725a0dbd3b186d93725ccce85cd1
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-11-14 13:46:45 -0800

    removed redundant folder and update db_con

--- flask_jwt_prod/db_con.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_db():
-    return psycopg2.connect(host="localhost", dbname="authme", user="chief", password="tacosgood")
-
-def get_db_instance():
-    db = get_db()
-    cur = db.cursor()
-    return db, cur
