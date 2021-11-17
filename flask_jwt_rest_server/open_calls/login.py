6c05dccabc1717a4f6be4888ecbc130746f4b905
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-11-17 14:47:35 -0800

    update login logic

--- flask_jwt_rest_server/open_calls/login.py
+++ flask_jwt_rest_server/open_calls/login.py
@@ -1,19 +1,35 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
+from psycopg2 import sql
 from tools.token_tools import create_token
 
 from tools.logging import logger
 
+import bcrypt
+
 def handle_request():
     logger.debug("Login Handle Request")
+    db = g.db
+    cur = db.cursor()
     #use data here to auth the user
-
     password_from_user_form = request.form['password']
     user = {
             "sub" : request.form['username'] #sub is used by pyJwt as the owner of the token
             }
-    if not user:
-        return json_response(status_=401, message = 'Invalid credentials', authenticated =  False )
-    logger.debug(f"User: {user}")
-    return json_response( token = create_token(user) , authenticated = False)
+    #sanitize the query
+    query = sql.SQL("select {field} from {table} where {pkey} = %s").format(
+      field=sql.Identifier('username'),
+      table=sql.Identifier('users'),
+      pkey=sql.Identifier('username'))
+    #check if user exists
+    cur.execute(query, (user['sub'],))     
+    if not cur.fetchone():
+        return json_response(message = "Incorrect username/password. Please try again.", authenticated = False)
+    else:
+        cur.execute(query, (user['sub'],)) 
+        hashed_pass = cur.fetchone()[2]
+        if bcrypt.checkpw(bytes(password_from_user_form, 'utf-8'), bytes(hashed_pass, 'utf-8')):
+            return json_response( token = create_token(user) , authenticated = True)
+        else:
+            return json_response(message = "Incorrect username/password. Please try again.", authenticated = False)
 
