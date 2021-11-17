007e9f394a2f4829250cbec6741bd4df329b5717
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-11-17 15:33:32 -0800

    moving script from html to js file

--- flask_jwt_rest_server/static/cis444.js
+++ flask_jwt_rest_server/static/cis444.js
@@ -1,4 +1,21 @@
 var jwt = null
+//initialize the page to show login
+$('#loginForm').show();
+$('div:not(#loginForm)').hide();
+
+/**
+ * swaps login and signup form
+ */
+function swaplogin() {
+	$('#loginForm').toggle();
+	$('#signupForm').toggle();
+}
+/**
+ * Gets get_books endpoint and assigns global JWT
+ * @param {string} endpoint 
+ * @param {function} on_success_callback 
+ * @param {function} on_fail_callback 
+ */
 function secure_get_with_token(endpoint, on_success_callback, on_fail_callback) {
 	xhr = new XMLHttpRequest();
 	function setHeader(xhr) {
@@ -19,6 +36,11 @@ function secure_get_with_token(endpoint, on_success_callback, on_fail_callback) 
 		beforeSend: setHeader
 	});
 }
+
+/**
+ * Sends form data to signup endpoint and displays create status based on success/failure
+ * @returns false
+ */
 function send_signup() {
 	$.post("/open_api/signup", { "username": $('#new_user').val(), "password": $('#new_pwd').val() },
 		function (textStatus) {
