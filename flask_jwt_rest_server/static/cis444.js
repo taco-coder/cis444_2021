901f4f137f9d0e7f14004412e2ede604c60f66cb
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-11-17 15:28:15 -0800

    moving signup to js file

--- flask_jwt_rest_server/static/cis444.js
+++ flask_jwt_rest_server/static/cis444.js
@@ -19,3 +19,15 @@ function secure_get_with_token(endpoint, on_success_callback, on_fail_callback) 
 		beforeSend: setHeader
 	});
 }
+function send_signup() {
+	$.post("/open_api/signup", { "username": $('#new_user').val(), "password": $('#new_pwd').val() },
+		function (textStatus) {
+			//this gets called when browser receives response from server
+			$('#create_status').html(textStatus.message)
+		}, "json").fail(function (response) {
+			//this gets called if the server throws an error
+			console.log("error");
+			console.log(response);
+		});
+	return false;
+}
