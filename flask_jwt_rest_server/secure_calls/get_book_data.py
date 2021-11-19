d94b04a632bc1eca2805263cab22f7035e9937bf
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-11-19 15:38:31 -0800

    debugging get data

--- flask_jwt_rest_server/secure_calls/get_book_data.py
+++ flask_jwt_rest_server/secure_calls/get_book_data.py
@@ -6,5 +6,5 @@ from tools.logging import logger
 
 def handle_request():
     logger.debug("Get Books Data Handle Request")
-    print(f"Book ID: {request.json}")
+    print(f"Book ID: {request.data}")
     return json_response( token = create_token(  g.jwt_data ) , info = {})
