c90521528259988462174ede26c0fd32768d3e05
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-11-20 13:55:45 -0800

    debugging

--- flask_jwt_rest_server/static/cis444.js
+++ flask_jwt_rest_server/static/cis444.js
@@ -29,12 +29,12 @@ function swaplogin() {
  */
 function secure_get_with_token(endpoint, data_to_send, on_success_callback, on_fail_callback) {
 	console.log("Secure Get")
+	console.log(data_to_send)
 	xhr = new XMLHttpRequest();
 	function setHeader(xhr) {
 		xhr.setRequestHeader('Authorization', 'Bearer:' + jwt);
 	}
 	function get_and_set_new_jwt(data) {
-		console.log("success callback")
 		console.log(data);
 		jwt = data.token
 		on_success_callback(data)
