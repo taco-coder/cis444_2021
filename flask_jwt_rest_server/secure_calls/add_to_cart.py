1f16b3ce07a38893f5257b4ea9f285fccb3ddec5
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-11-20 17:46:57 -0800

    added backend code for cart

--- flask_jwt_rest_server/secure_calls/add_to_cart.py
+++ flask_jwt_rest_server/secure_calls/add_to_cart.py
@@ -1,4 +1,4 @@
-from flask import request, g
+from flask import request, g, session
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from tools.token_tools import create_token
 
@@ -7,10 +7,9 @@ from tools.logging import logger
 def handle_request():
     logger.debug("Post Book Review Handle Request")
 
-    cart = g.cart
-    print(cart)
-    cart.append({request.form.get('name') : request.form.get('price')})
-    print(cart)
-    g.cart = cart
+    if 'cart' in session:
+      session['cart'].append({request.form.get('name') : request.form.get('price')})
+    else:
+      session['cart'] = {request.form.get('name') : request.form.get('price')}
 
-    return json_response( token = create_token( g.jwt_data ), user_cart = cart )
+    return json_response( token = create_token( g.jwt_data ), user_cart = session['cart'] )
