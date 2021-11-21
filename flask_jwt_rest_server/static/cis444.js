21804096c6fac0412cdf2e08c07403e1803afaf1
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-11-20 16:31:49 -0800

    calculating avg

--- flask_jwt_rest_server/static/cis444.js
+++ flask_jwt_rest_server/static/cis444.js
@@ -275,7 +275,11 @@ function postReview(book_id) {
 
 			//calc new avg rating
 			var numReviews = ($(userR + " br").length) / 2; //there are 2 line breaks after each review; getting num of reviews based on line breaks
+			console.log(numReviews)
+			console.log(avgRate * numReviews)
+			console.log($(rating).val())
 			//get rough sum of all the previous ratings, add the current rating, then div by num of previous reviews plus this new one
+
 			avgRate = ((avgRate * numReviews) + $(rating).val()) / (numReviews + 1);
 			$(rate).html("Rating: " + avgRate.toFixed(1) + "/5")
 		}, function (err) {
