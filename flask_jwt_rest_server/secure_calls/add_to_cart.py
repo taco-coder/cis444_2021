9df88e25d7339a2e5c21de8f2a27b55535f9ff29
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2021-11-20 18:16:25 -0800

    dead code

--- flask_jwt_rest_server/secure_calls/add_to_cart.py
+++ flask_jwt_rest_server/secure_calls/add_to_cart.py
@@ -8,13 +8,13 @@ from tools.logging import logger
 def handle_request():
     logger.debug("Add to Cart Handle Request")
     if 'book_name' in session:
-        session['book_name'] = session.get('book_name') + ";" + request.form['book_name'] 
+        session['book_name'] = session.get('book_name') + ";" + request.form['name'] 
     else:
-        session['book_name'] = request.form['book_name']
+        session['book_name'] = request.form['name']
     if 'book_price' in session:
-        session['book_price'] = session.get('book_price') + ";" + request.form['book_price'] 
+        session['book_price'] = session.get('book_price') + ";" + request.form['price'] 
     else:
-        session['book_price'] = request.form['book_price'] 
+        session['book_price'] = request.form['price'] 
 
 
     return json_response( token = create_token( g.jwt_data ))
